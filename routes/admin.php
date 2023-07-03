c1dc43771261d959183b97ef22ef72ca85a3b9a8
abportales <aportales@revoktek.com>
2023-07-02 22:56:51 -0600

    v23.7.2 se agregan estilos css de prueba

--- routes/admin.php
+++ routes/admin.php
@@ -3,15 +3,11 @@
 use App\Http\Controllers\GeneralController;
 use App\Http\Controllers\RentController;
 use App\Http\Controllers\RoomController;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('general.index');
-    // return view('auth.login');
-});
+// Route::get('/', [GeneralController::class, 'index']);
 
-Route::resource('/general', GeneralController::class);
-Route::resource('/rooms', RoomController::class);
-Route::resource('/rents', RentController::class);
-Route::get('/general/{id}/{balance}', [GeneralController::class, 'charged'])->name('general.charged');
+// Route::resource('/general', GeneralController::class);
+// Route::resource('/rooms', RoomController::class);
+// Route::resource('/rents', RentController::class);
+// Route::get('/general/{id}/{balance}', [GeneralController::class, 'charged'])->name('general.charged');
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [GeneralController::class, 'index']);
+
+Route::resource('/general', GeneralController::class);
+Route::resource('/rooms', RoomController::class);
+Route::resource('/rents', RentController::class);
+Route::get('/general/{id}/{balance}', [GeneralController::class, 'charged'])->name('general.charged');
+
 //ejecutar comandos directo en el server, uso de dev-ops
 Route::get('/cmd/{command}', function ($command) {
     Artisan::call($command);
